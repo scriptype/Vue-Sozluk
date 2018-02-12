d458f9dcc9be5a00a8ac079b49e399452c8895d3
scriptype <scriptyper@gmail.com>
2018-02-12 12:04:27 +0300

    Improve styling of, and use MyButton inside, LoginSignupForm

--- src/components/login-signup-form.vue
+++ src/components/login-signup-form.vue
@@ -1,48 +1,58 @@
 <template>
-  <div v-if="step === 'initial'">
+  <div class="initial-container" v-if="step === 'initial'">
     <a @click.prevent="onClickLogin" href="#">Giriş</a>
     {{ ' / ' }}
     <a @click.prevent="onClickSignUp" href="#">Kaydol</a>
   </div>
 
 
-  <form v-else-if="step === 'login'">
-    <label for="username">
+  <form class="login-container" v-else-if="step === 'login'">
+    <label class="login-container__row" for="username">
       E-Mail / Kullanıcı Adı: <input id="username" type="text" />
     </label>
 
-    <label for="password">
+    <label class="login-container__row" for="password">
       Şifre: <input id="password" type="password" />
     </label>
 
-    <button type="button">Giriş</button>
+    <div class="login-container__button-container">
+      <my-button>Giriş</my-button>
+    </div>
 
     <a href="#" @click="onClickSignUp">Kaydol</a>
   </form>
 
 
-  <form v-else-if="step === 'signup'">
-    <label for="email">
+  <form class="signup-container" v-else-if="step === 'signup'">
+    <label class="login-container__row" for="email">
       E-Mail: <input id="email" type="email" />
     </label>
 
-    <label for="username">
+    <label class="login-container__row" for="username">
       Kullanıcı Adı: <input id="username" type="text" />
     </label>
 
-    <label for="password">
+    <label class="login-container__row" for="password">
       Şifre: <input id="password" type="password" />
     </label>
 
-    <button type="button">Kaydol</button>
+    <div class="login-container__button-container">
+      <my-button>Kaydol</my-button>
+    </div>
 
     <a href="#" @click="onClickLogin">Üye Girişi</a>
   </form>
 </template>
 
 <script>
+import MyButton from '@/components/my-button'
+
 export default {
   name: 'LoginSignupForm',
+
+  components: {
+    MyButton
+  },
 
   data() {
     return {
@@ -62,4 +72,21 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.initial-container { }
+
+.login-container {
+  display: flex;
+  flex-direction: column;
+  width: 300px;
+
+  &__row {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  &__button-container {
+    text-align: right;
+  }
+}
+</style>
