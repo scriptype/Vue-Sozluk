888e6edf7dff95075a0efa835c3dae171494ceb4
scriptype <scriptyper@gmail.com>
2018-02-12 10:40:52 +0300

    Implement login/signup ui state change

--- src/components/login-signup-form.vue
+++ src/components/login-signup-form.vue
@@ -1,22 +1,64 @@
 <template>
-  <div>
+  <div v-if="step === 'initial'">
     <a @click.prevent="onClickLogin" href="#">Giriş</a>
     {{ ' / ' }}
     <a @click.prevent="onClickSignUp" href="#">Kaydol</a>
   </div>
+
+
+  <form v-else-if="step === 'login'">
+    <label for="username">
+      E-Mail / Kullanıcı Adı: <input id="username" type="text" />
+    </label>
+
+    <label for="password">
+      Şifre: <input id="password" type="password" />
+    </label>
+
+    <button type="button">Giriş</button>
+
+    <a href="#" @click="onClickSignUp">Kaydol</a>
+  </form>
+
+
+  <form v-else-if="step === 'signup'">
+    <label for="email">
+      E-Mail: <input id="email" type="email" />
+    </label>
+
+    <label for="username">
+      Kullanıcı Adı: <input id="username" type="text" />
+    </label>
+
+    <label for="password">
+      Şifre: <input id="password" type="password" />
+    </label>
+
+    <button type="button">Kaydol</button>
+
+    <a href="#" @click="onClickLogin">Üye Girişi</a>
+  </form>
 </template>
 
 <script>
 export default {
   name: 'LoginSignupForm',
+
+  data() {
+    return {
+      step: 'initial'
+    }
+  },
+
   methods: {
     onClickLogin() {
-      console.log('clicked login')
+      this.step = 'login'
     },
     onClickSignUp() {
-      console.log('clicked signup')
+      this.step = 'signup'
     }
   }
+
 }
 </script>
 
