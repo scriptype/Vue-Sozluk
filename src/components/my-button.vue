174c41b1e48787d74fbcaf383994c7b034fc91de
scriptype <scriptyper@gmail.com>
2018-02-12 12:03:37 +0300

    Create MyButton component

---
+++ src/components/my-button.vue
@@ -0,0 +1,94 @@
+<template>
+  <button
+    :class="buttonClassObject"
+    type="button"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'MyButton',
+
+  props: {
+    type: {
+      type: String,
+      default: ''
+    },
+    size: {
+      type: String,
+      default: 'auto'
+    },
+    level: {
+      type: String,
+      default: 'primary'
+    }
+  },
+
+  computed: {
+    buttonClassObject() {
+      return {
+        button: true,
+        'button--error': this.$props.type === 'error',
+        'button--danger': this.$props.type === 'danger',
+        'button--success': this.$props.type === 'success',
+        'button--big': this.$props.size === 'big',
+        'button--fluid': this.$props.size === 'fluid',
+        'button--primary': this.$props.level === 'primary',
+        'button--secondary': this.$props.level === 'secondary',
+        'button--tertiary': this.$props.level === 'tertiary'
+      }
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import "../styles/helpers/variables";
+
+.button {
+  padding: .2em .8em;
+  border: none;
+  border-radius: 3px;
+  font: inherit;
+}
+
+.button--primary {
+  background: $primary-bg;
+  color: $primary-fg;
+}
+
+.button--secondary {
+  background: $secondary-bg;
+  color: $secondary-fg;
+}
+
+.button--tertiary {
+  background: $tertiary-bg;
+  color: $tertiary-fg;
+}
+
+.button--error {
+  background: $error-bg;
+  color: $error-fg;
+}
+
+.button--danger {
+  background: $danger-bg;
+  color: $danger-fg;
+}
+
+.button--success {
+  background: $success-bg;
+  color: $success-fg;
+}
+
+.button--big {
+  padding: .4em 1.6em;
+}
+
+.button--fluid {
+  width: 100%;
+}
+</style>
