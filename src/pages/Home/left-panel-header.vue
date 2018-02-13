83c0252d7fe68a74310cc17b4c1b2c720f37d80e
scriptype <scriptyper@gmail.com>
2018-02-13 18:14:27 +0300

    Create Home/LeftPanelHeader component

---
+++ src/pages/Home/left-panel-header.vue
@@ -0,0 +1,26 @@
+<template>
+  <h3 class="left-panel-header">Başlıklar</h3>
+</template>
+
+
+<script>
+export default {
+  name: 'LeftPanelHeader'
+}
+</script>
+
+
+<style scoped lang="scss">
+@import "../../styles/helpers/variables";
+@import "../../styles/helpers/responsive";
+
+.left-panel-header {
+  padding: .5em;
+  background: $secondary-bg;
+  color: $secondary-fg;
+
+  @media (max-width: $sm-max) {
+    padding: 1em;
+  }
+}
+</style>
