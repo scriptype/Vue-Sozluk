646120150293c4c563866d6ecd4047e49eaf9b6f
scriptype <scriptyper@gmail.com>
2018-02-13 15:03:45 +0300

    Create Home/LeftPanel component

---
+++ src/pages/Home/left-panel.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="left-panel">left</div>
+</template>
+
+
+<script>
+export default {
+  name: 'LeftPanel'
+}
+</script>
+
+
+<style scoped lang="scss">
+@import "../../styles/helpers/variables";
+@import "../../styles/helpers/responsive";
+</style>
