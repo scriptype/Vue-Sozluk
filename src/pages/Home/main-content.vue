46f39c64804f4f6db76532076d86f90a0e0a42db
scriptype <scriptyper@gmail.com>
2018-02-14 10:23:02 +0300

    Create Home/MainContent component

---
+++ src/pages/Home/main-content.vue
@@ -0,0 +1,41 @@
+<template>
+  <main class="main-content">
+    <transition mode="out-in" name="loading-topic">
+
+      <div v-if="!topic.id" class="loading-indicator-container">
+        <loading-indicator />
+      </div>
+
+      <div v-else class="topic">
+        <h1>{{ topic.title }}</h1>
+      </div>
+
+    </transition>
+  </main>
+</template>
+
+
+<script>
+import LoadingIndicator from '@/components/loading-indicator'
+
+export default {
+  name: 'MainContent',
+
+  components: {
+    LoadingIndicator
+  },
+
+  props: {
+    topic: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+
+<style scoped lang="scss">
+@import "../../styles/helpers/variables";
+@import "../../styles/helpers/responsive";
+</style>
