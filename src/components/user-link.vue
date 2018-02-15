b4c8bda82fbd4b71af59c52786555752c9ef9e78
scriptype <scriptyper@gmail.com>
2018-02-15 09:39:01 +0300

    Create UserLink component

---
+++ src/components/user-link.vue
@@ -0,0 +1,24 @@
+<template>
+  <router-link :to="getProfileUrl()">{{ user.name }}</router-link>
+</template>
+
+<script>
+export default {
+  name: 'UserLink',
+
+  props: {
+    user: {
+      type: Object,
+      required: true
+    }
+  },
+
+  methods: {
+    getProfileUrl() {
+      return `profile/${this.user.name}--${this.user.id}`
+    }
+  }
+}
+</script>
+
+<style scoped></style>
