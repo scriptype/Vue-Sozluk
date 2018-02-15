ba737baa9cce2bb8e27ad732dc7c1ffa9d5c3293
scriptype <scriptyper@gmail.com>
2018-02-15 10:29:37 +0300

    Create CreateEntryForm component

---
+++ src/components/create-entry-form.vue
@@ -0,0 +1,40 @@
+<template>
+  <form class="create-entry-form">
+    <textarea v-model="newEntryContent"></textarea>
+    <my-button
+      @click="onCreateEntry"
+      type="primary"
+      size="big"
+    >
+      Oluştur
+    </my-button>
+  </form>
+</template>
+
+
+<script>
+import MyButton from '@/components/my-button'
+
+export default {
+  name: 'CreateEntryForm',
+
+  components: {
+    MyButton
+  },
+
+  data() {
+    return {
+      newEntryContent: ''
+    }
+  },
+
+  methods: {
+    onCreateEntry() {
+      this.$emit('createEntry', this.newEntryContent)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+<style>
